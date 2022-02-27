d57e68bbea71c5e2a239e8d782def653ba4f7f56
NoyPhilosof <96237612+NoyPhilosof@users.noreply.github.com>
2022-02-27 22:22:47 +0200

    added variables.tf for username, password & region

---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "region" {
+    default = "eu-central-1"
+}
+
+variable "access_key" {
+    default = "AKIATUZWA65FI5UQDTX5"
+}
+
+variable "secret_key" {
+    default = "xUsyP+mKqdSUnlnoVZ3fanYb8qizSDljC0DfGThQ"
+}
